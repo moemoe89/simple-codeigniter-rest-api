5e9386d8aff8c45ef9154245c91c03197d5a1b7e
Bismo Baruno <bismobaruno@go-jek.com>
2015-12-27 17:37:49 +0700

    rollback construct not working

--- application/controllers/Book.php
+++ application/controllers/Book.php
@@ -6,7 +6,6 @@ class Book extends CI_Controller {
 	public function __construct()
     {
         parent::__construct();
-        $this->MyModel->check_auth_client();
     }
 
 	public function index()
@@ -15,13 +14,18 @@ class Book extends CI_Controller {
 		if($method != 'GET'){
 			json_output(400,array('status' => 400,'message' => 'Bad request.'));
 		} else {
-	        $response = $this->MyModel->auth();
-	        if($response['status'] == 200){
-	        	$resp = $this->MyModel->book_all_data();
-	        } else {
-	        	$resp = $response;
-	        } 
-    		json_output($response['status'],$resp);
+			$check_auth_client = $this->MyModel->check_auth_client();
+			if($check_auth_client == true){
+		        $response = $this->MyModel->auth();
+		        if($response['status'] == 200){
+		        	$resp = $this->MyModel->book_all_data();
+		        } else {
+		        	$resp = $response;
+		        } 
+	    		json_output($response['status'],$resp);
+			} else {
+				json_output(401,array('status' => 401,'message' => 'Unauthorized.'));
+			}
 		}
 	}
 
@@ -31,13 +35,18 @@ class Book extends CI_Controller {
 		if($method != 'GET' || $this->uri->segment(3) == '' || is_numeric($this->uri->segment(3)) == FALSE){
 			json_output(400,array('status' => 400,'message' => 'Bad request.'));
 		} else {
-	        $response = $this->MyModel->auth();
-	        if($response['status'] == 200){
-	        	$resp = $this->MyModel->book_detail_data($id);
-	        } else {
-	        	$resp = $response;
-	        }
-			json_output($response['status'],$resp);
+			$check_auth_client = $this->MyModel->check_auth_client();
+			if($check_auth_client == true){
+		        $response = $this->MyModel->auth();
+		        if($response['status'] == 200){
+		        	$resp = $this->MyModel->book_detail_data($id);
+		        } else {
+		        	$resp = $response;
+		        }
+				json_output($response['status'],$resp);
+			} else {
+				json_output(401,array('status' => 401,'message' => 'Unauthorized.'));
+			}
 		}
 	}
 
@@ -47,20 +56,25 @@ class Book extends CI_Controller {
 		if($method != 'POST'){
 			json_output(400,array('status' => 400,'message' => 'Bad request.'));
 		} else {
-	        $response = $this->MyModel->auth();
-	        $respStatus = $response['status'];
-	        if($response['status'] == 200){
-				$params = json_decode(file_get_contents('php://input'), TRUE);
-				if ($params['title'] == "" || $params['author'] == "") {
-					$respStatus = 400;
-					$resp = array('status' => 400,'message' =>  'Title & Author can\'t empty');
-				} else {
-	        		$resp = $this->MyModel->book_create_data($params);
-				}
-	        } else {
-	        	$resp = $response;
-	        }
-			json_output($respStatus,$resp);
+			$check_auth_client = $this->MyModel->check_auth_client();
+			if($check_auth_client == true){
+		        $response = $this->MyModel->auth();
+		        $respStatus = $response['status'];
+		        if($response['status'] == 200){
+					$params = json_decode(file_get_contents('php://input'), TRUE);
+					if ($params['title'] == "" || $params['author'] == "") {
+						$respStatus = 400;
+						$resp = array('status' => 400,'message' =>  'Title & Author can\'t empty');
+					} else {
+		        		$resp = $this->MyModel->book_create_data($params);
+					}
+		        } else {
+		        	$resp = $response;
+		        }
+				json_output($respStatus,$resp);
+			} else {
+				json_output(401,array('status' => 401,'message' => 'Unauthorized.'));
+			}
 		}
 	}
 
@@ -70,21 +84,26 @@ class Book extends CI_Controller {
 		if($method != 'PUT' || $this->uri->segment(3) == '' || is_numeric($this->uri->segment(3)) == FALSE){
 			json_output(400,array('status' => 400,'message' => 'Bad request.'));
 		} else {
-	        $response = $this->MyModel->auth();
-	        $respStatus = $response['status'];
-	        if($response['status'] == 200){
-				$params = json_decode(file_get_contents('php://input'), TRUE);
-				$params['updated_at'] = date('Y-m-d H:i:s');
-				if ($params['title'] == "" || $params['author'] == "") {
-					$respStatus = 400;
-					$resp = array('status' => 400,'message' =>  'Title & Author can\'t empty');
-				} else {
-	        		$resp = $this->MyModel->book_update_data($id,$params);
-				}
-	        } else {
-	        	$resp = $response;
-	        }
-			json_output($respStatus,$resp);
+			$check_auth_client = $this->MyModel->check_auth_client();
+			if($check_auth_client == true){
+		        $response = $this->MyModel->auth();
+		        $respStatus = $response['status'];
+		        if($response['status'] == 200){
+					$params = json_decode(file_get_contents('php://input'), TRUE);
+					$params['updated_at'] = date('Y-m-d H:i:s');
+					if ($params['title'] == "" || $params['author'] == "") {
+						$respStatus = 400;
+						$resp = array('status' => 400,'message' =>  'Title & Author can\'t empty');
+					} else {
+		        		$resp = $this->MyModel->book_update_data($id,$params);
+					}
+		        } else {
+		        	$resp = $response;
+		        }
+				json_output($respStatus,$resp);
+			} else {
+				json_output(401,array('status' => 401,'message' => 'Unauthorized.'));
+			}
 		}
 	}
 
@@ -94,13 +113,18 @@ class Book extends CI_Controller {
 		if($method != 'DELETE' || $this->uri->segment(3) == '' || is_numeric($this->uri->segment(3)) == FALSE){
 			json_output(400,array('status' => 400,'message' => 'Bad request.'));
 		} else {
-	        $response = $this->MyModel->auth();
-	        if($response['status'] == 200){
-	        	$resp = $this->MyModel->book_delete_data($id);
-	        } else {
-	        	$resp = $response;
-	        }
-			json_output($response['status'],$resp);
+			$check_auth_client = $this->MyModel->check_auth_client();
+			if($check_auth_client == true){
+		        $response = $this->MyModel->auth();
+		        if($response['status'] == 200){
+		        	$resp = $this->MyModel->book_delete_data($id);
+		        } else {
+		        	$resp = $response;
+		        }
+				json_output($response['status'],$resp);
+			} else {
+				json_output(401,array('status' => 401,'message' => 'Unauthorized.'));
+			}
 		}
 	}
 
